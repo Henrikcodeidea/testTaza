eb3f8028b3824d9dfbf4cf288225f78a7f1e9f8b
Gevorg Martirosyan <gevorg.martirosyan.work@gmail.com>
2022-03-28 13:40:43 +0400

    FIR-14

--- test.php
+++ test.php
@@ -1,4 +1,3 @@
 <?php
 return 1;
-?>ttyttSDASDASDASDAAAAAAAAAAAasdddddddddddddddddddddddddddddddd
-asdasdasdadasdasdaasdasasdasdsadasdasdasdasdasddasdasdasdasdasdasxcfasfcadasdaqasdasdasdasdaasdasdasdd
+?>
